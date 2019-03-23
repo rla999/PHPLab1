2e54d053d7b2edd41ceaf0828fd3d863c038229c
rla999 <rlasher1999@gmail.com>
2019-03-23 15:16:04 -0400

    final version of lab 1

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!--
+Ryan Lasher
+PHP -- CSC 257
+02/02/2019
+Lab 01
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Lab 01 - Ryan Lasher Web Dev with PHP</title>
+        <style>
+            body {background-color: MediumAquaMarine;}
+            h1   {color: blue;text-align: center;}
+            h2   {color: darkblue;text-align: center;}
+            p    {font-size:115%;}
+            ol   {color: red;font-size:200%;border: 1px solid powderblue;}
+            footer {text-align: center;}
+        </style>
+    </head>
+    <body>
+        <h1>Top 10 Largest Cities in the World</h1>
+        <h2>By Population</h2>
+        <p> Here are the top 10 largest cities in the world, by population.</p>
+        <ol reversed>
+            <li><strong>Mumbai, India - 12.4 million</strong></li>
+            <img alt="10 - Mumbai" src="images/10-mumbai.jpg"/>
+            <li><strong>Tianjin, China - 12.7 million</strong></li>
+            <img alt="9 - Tianjin" src="images/9-Tianjin.jpg"/>
+            <li><strong>Manila, The Philippines - 12.8 million</strong></li>
+            <img alt="8 - Manila" src="images/8-Manila.jpg"/>
+            <li><strong>Moscow, Russia - 13.1 million</strong></li>
+            <img alt="7 - Moscow" src="images/7-Moscow.jpg"/>
+            <li><strong>Tokyo, Japan - 13.6 million</strong></li>
+            <img alt="6 - Tokyo" src="images/6-Tokyo.jpg"/>
+            <li><strong>Dhaka, Bangladesh - 14.5 million</strong></li>
+            <img alt="5 - Dhaka" src="images/5-Dhaka.jpg"/>
+            <li><strong>Istanbul, Turkey - 14.6 million</strong></li>
+            <img alt="4 - Istanbul" src="images/4-Istanbul.jpg"/>
+            <li><strong>Karachi, Pakistan - 18 million</strong></li>
+            <img alt="3 - Karachi" src="images/3-Karachi.jpg"/>
+            <li><strong>Beijing, China - 18.5 million</strong></li>
+            <img alt="2 - Beijing" src="images/2-Beijing.jpg"/>
+            <li><strong>Shanghai, China - 24.1 million</strong></li>
+            <img alt="1 - Shanghai" src="images/1-Shanghai.jpg"/>
+        </ol>
+        <p><a href = "https://www.worldatlas.com/articles/the-10-largest-cities-in-the-world.html"> <strong>Source</strong></a></p>
+        <footer>
+            Copyright (c) 2019 Ryan Lasher. All Rights Reserved.
+        </footer>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
